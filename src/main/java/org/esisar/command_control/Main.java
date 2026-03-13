c52e9d2c0cc52ac691fb56ce641effd5a1b14a9d
Quentin LESPOUR <52582750+Yapudco@users.noreply.github.com>
2026-03-13 22:39:35 +0100

    un poc presque complet

--- src/main/java/org/esisar/command_control/Main.java
+++ src/main/java/org/esisar/command_control/Main.java
@@ -1,27 +1,84 @@
 package org.esisar.command_control;
 
 import io.javalin.Javalin;
+import org.esisar.command_control.encrypt.AES;
+import org.esisar.command_control.encrypt.RSA;
+import org.esisar.command_control.server_logic.Client;
 
-
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
-        Javalin app = Javalin.create().start(7000);
+       // Javalin app = Javalin.create().start(7000);
+
+        Javalin app = Javalin.create(config -> {
+            config.jetty.modifyWebSocketServletFactory(factory -> {
+                factory.setIdleTimeout(Duration.ofHours(2)); // on définit le temps de Timeout à 2heures pour pas etre emmerdé
+            });
+        }).start(7000);
+
+        ArrayList<Client> clients = new ArrayList<>();
 
         //j'ai changé d'avis je fais du websocket
-        //et celui qui m'en empèche est un creuvard
+
         app.ws("/", ws -> {
             ws.onConnect(ctx ->{
+
                 System.out.println("Machine " + ctx.sessionId() + " connected");
-                ctx.send("ceci est un test");
+                Client client = new Client(ctx);
+                clients.add(client);
+
+
+                // On gère la logique du "HandShake"
+                RSA rsa = new RSA();
+                AES aes = new AES();
+
+
             });
+
+            ws.onMessage(ctx -> {
+                //Gestion anti déconection => obligé pour éviter que les rooters nous emmerdent.
+                if(ctx.message().equals("Ping")){
+                    System.out.println("Pong");
+                    ctx.send("Pong");
+                }else{
+                    System.out.println(ctx.message());
+                }
+
+            });
+
+            //
             ws.onClose(ctx ->{
-                System.out.println("Machine " + ctx.sessionId() + " closed");
+                System.out.println("Machine " + ctx.sessionId() + " disconnected");
+                for(Client client : clients){
+                    if (ctx.sessionId().equals(client.getSessionId())) {clients.remove(client);}
+                    //client... client... CLIENT AAAAAAAAAAAAAAAAAAAA
+                }
             });
         });
 
 
+        // Le parseur  (désolé Béatrice <3)
+        Thread consoleReader = new Thread(() -> {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("> Entrer Commande");
 
+            while (true) {
+                if (scanner.hasNextLine()) {
+                    String message = scanner.nextLine();
+                    for(Client client : clients){
+                        client.sendMessage(message);
+                    }
+
+                }
+            }
+        });
+
+
+        consoleReader.setDaemon(true); // S'arrête si le serveur s'arrête
+        consoleReader.start();
     }
 }
---
+++ src/main/java/org/esisar/command_control/encrypt/AES.java
@@ -0,0 +1,10 @@
+package org.esisar.command_control.encrypt;
+
+import io.javalin.websocket.WsContext;
+
+public class AES {
+    public AES() {}
+    public String encrypt(String text){return text;}
+    public void decrypt(String text){}
+    public void getPublicKey(){}
+}
