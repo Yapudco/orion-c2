04d85b6470e38adc1c648c50ffd753ff053ba730
kora <bastien1602f@gmail.com>
2026-03-15 21:33:11 +0100

    .

--- src/main/java/org/esisar/command_control/Parser.java
+++ src/main/java/org/esisar/command_control/Parser.java
@@ -1,15 +1,18 @@
 package org.esisar.command_control;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Parser {
+    public static java.io.File currentFile = new java.io.File(System.getProperty("user.dir"));
     public static void main(String[] arg) throws IOException {
 
-        String fausseCommande = "whoami";
+        String fausseCommande = "cd C:\\Users\\basti\\Desktop\\ && mkdir test";
         System.out.println("On envoie au parseur : " + fausseCommande);
 
         List<String> command = commandSplit(fausseCommande);
@@ -37,7 +40,17 @@ public class Parser {
         return result;
     }
     public static String executeCommand(List<String> command) throws IOException {
-        Process execute = new ProcessBuilder(command).start();
+
+        if (command.get(0).equals("cd")){
+            if (command.size() > 1) {
+                currentFile = new java.io.File(command.get(1));
+                return "moved into " + currentFile.getAbsolutePath() + "\n";
+            }else{
+                return "Error : cd need path" + "\n";
+            }
+
+        }
+        Process execute = new ProcessBuilder(command).directory(currentFile).start();
         java.io.InputStream flux = execute.getInputStream();
         java.io.BufferedReader lecteur = new java.io.BufferedReader(new java.io.InputStreamReader(flux));
 
