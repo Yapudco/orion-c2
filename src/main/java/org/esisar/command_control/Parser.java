3bb2facba065886e1be41f07fad79f47825e7203
kora <bastien1602f@gmail.com>
2026-03-15 15:36:21 +0100

    Création de l'execution de commande
    Début de la création de la méthode pour executer les commandes. Ajout de la nouvelle version de java et de ses dépendances.

--- src/main/java/org/esisar/command_control/Parser.java
+++ src/main/java/org/esisar/command_control/Parser.java
@@ -1,15 +1,14 @@
 package org.esisar.command_control;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Parser {
-    List<String> result = new ArrayList<>(); //making a list
-    String message;
+    public static void main(String[] arg) {
 
-    public static void main(String[] arg){
         String fausseCommande = "download \"C:\\Mes Documents Secrets\\passwords.txt\"   /tmp/loot.txt";
         System.out.println("On envoie au parseur : " + fausseCommande);
 
@@ -35,15 +34,10 @@ public class Parser {
             System.out.println("Case n°" + i + " -> [" + result.get(i) + "]");
         }
         return result;
-
-
-
-
-
-
-
-
-
+    }
+    public static void executeCommand(List<String> command) throws IOException {
+        Process execute = new ProcessBuilder("java", "11").start();
+        execute.getInputStream();
 
 
     }
